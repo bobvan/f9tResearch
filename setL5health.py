7f10f18abf059fa1ca29d6b9d6ca950ba11ddb19
Bob Van Valzah <Bob@VanValzah.Com>
2025-07-30 15:47:11 -0500

    Code and first data run for L1/L5 fixed pos

---
+++ setL5health.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from serial import Serial
+from pyubx2 import UBXMessage, UBXReader, val2sphp, SET, SET_LAYER_RAM, TXN_NONE
+
+# Adjust for your setup
+PORT = "/dev/ttyACM0"  # Or COMx on Windows
+BAUD = 9600
+
+
+with Serial(PORT, BAUD, timeout=2) as ser:
+#    l5healthMsg = UBXReader.parse(b'\xB5\x62\x06\x8A\x09\x00\x00\x01\x00\x00\x01\x00\x32\x10\x01\xDE\xED') # From UBX-21038688 - R03
+#    print(l5healthMsg)
+#    ser.write(l5healthMsg.serialize())
+    ser.write(b'\xB5\x62\x06\x8A\x09\x00\x00\x01\x00\x00\x01\x00\x32\x10\x01\xDE\xED') # From UBX-21038688 - R03
+
+    ubr = UBXReader(ser, protfilter=2)
+    while True:
+        (raw_data, parsed_data) = ubr.read()
+        print(parsed_data.identity, parsed_data)
+        if parsed_data and parsed_data.identity == 'ACK-ACK':
+            print("L5 health set in RAM.")
+            break
+        if parsed_data and parsed_data.identity == 'ACK-NAK':
+            print("Attempt to set L5 health")
+            break
--- setSignal.py
+++ setSignal.py
@@ -11,43 +11,54 @@ with Serial(PORT, BAUD, timeout=2) as ser:
     # Construct CFG-VALSET message
     sigEnaMsg = UBXMessage.config_set(SET_LAYER_RAM, TXN_NONE,
         [
+        # Enable Only GPS
             ("CFG_SIGNAL_BDS_ENA", 0),
+            ("CFG_SIGNAL_GAL_ENA", 0),
+            ("CFG_SIGNAL_GLO_ENA", 0),
+            ("CFG_SIGNAL_GPS_ENA", 1),
+            ("CFG_SIGNAL_GPS_L1CA_ENA", 1),
+            ("CFG_SIGNAL_GPS_L5_ENA", 1),
+            ("CFG_SIGNAL_NAVIC_ENA", 0),
+            ("CFG_SIGNAL_QZSS_ENA", 0),
+            ("CFG_SIGNAL_SBAS_ENA", 0),
+
+#            ("CFG_SIGNAL_BDS_ENA", 0),
 #            ("CFG_SIGNAL_BDS_B1C_ENA", 0),
 #            ("CFG_SIGNAL_BDS_B1_ENA", 0),
 #            ("CFG_SIGNAL_BDS_B2A_ENA", 0),
 #            ("CFG_SIGNAL_BDS_B2_ENA", 0),
 ##            ("CFG_SIGNAL_BDS_B3_ENA", 0),
 
-            ("CFG_SIGNAL_GAL_ENA", 0),
+#            ("CFG_SIGNAL_GAL_ENA", 0),
 #            ("CFG_SIGNAL_GAL_E1_ENA", 0),
 #            ("CFG_SIGNAL_GAL_E5A_ENA", 0),
 #            ("CFG_SIGNAL_GAL_E5B_ENA", 0),
 ##            ("CFG_SIGNAL_GAL_E6_ENA", 0),
 
-            ("CFG_SIGNAL_GLO_ENA", 0),
+#            ("CFG_SIGNAL_GLO_ENA", 0),
 #            ("CFG_SIGNAL_GLO_L1_ENA", 0),
 #            ("CFG_SIGNAL_GLO_L2_ENA", 0),
 
-            ("CFG_SIGNAL_GPS_ENA", 1),
-            ("CFG_SIGNAL_GPS_L1CA_ENA", 1),
+#            ("CFG_SIGNAL_GPS_ENA", 1),
+#            ("CFG_SIGNAL_GPS_L1CA_ENA", 1),
 #            ("CFG_SIGNAL_GPS_L2C_ENA", 1),
 #            ("CFG_SIGNAL_GPS_L5_ENA", 1),
 ##            ("CFG_SIGNAL_HEALTH_L5", 0),
 
-#            ("CFG_SIGNAL_IMES_ENA", 0),
+#            ("CFG_SIGNAL_IMES_ENA", 0),    # This disable always seems to fail
 #            ("CFG_SIGNAL_IMES_L1_ENA", 0),
 
 #            ("CFG_SIGNAL_NAVIC_ENA", 0),
 #            ("CFG_SIGNAL_NAVIC_L5_ENA", 0),
 ##            ("CFG_SIGNAL_PLAN", 0),
 
-            ("CFG_SIGNAL_QZSS_ENA", 0),
+#            ("CFG_SIGNAL_QZSS_ENA", 0),
 #            ("CFG_SIGNAL_QZSS_L1CA_ENA", 0),
 #            ("CFG_SIGNAL_QZSS_L1S_ENA", 0),
 #            ("CFG_SIGNAL_QZSS_L2C_ENA", 0),
 #            ("CFG_SIGNAL_QZSS_L5_ENA", 0),
 
-            ("CFG_SIGNAL_SBAS_ENA", 0),
+#            ("CFG_SIGNAL_SBAS_ENA", 0),
 #            ("CFG_SIGNAL_SBAS_L1CA_ENA", 0),
         ]
     )
